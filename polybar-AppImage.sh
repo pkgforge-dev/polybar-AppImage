d988ded863ed505e214c7844a5429ea4c59e18d6
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-05-23 18:14:57 -0400

    Update polybar-AppImage.sh

--- polybar-AppImage.sh
+++ polybar-AppImage.sh
@@ -52,7 +52,7 @@ ln -s ./polybar.png ./.DirIcon
 
 # MAKE APPIMAGE
 LINUXDEPLOY="https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-static-x86_64.AppImage"
-cd .. && wget "$LINUXDEPLOY" -O linuxdeploy && chmod a+x ./linuxdeploy && NO_STRIP=true ./linuxdeploy --appdir "$APPDIR" --executable "$APPDIR"/usr/bin/"$EXEC" --output appimage
+cd .. && wget "$LINUXDEPLOY" -O linuxdeploy && chmod a+x ./linuxdeploy && ./linuxdeploy --appdir "$APPDIR" --executable "$APPDIR"/usr/bin/"$EXEC" --output appimage
 
 # LIBFUSE3
 APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')
