73774ed162c8bcef88ae2e9df6861a59589ac0c2
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-08-06 03:58:54 -0400

    Update polybar-AppImage.sh

--- polybar-AppImage.sh
+++ polybar-AppImage.sh
@@ -7,7 +7,7 @@ REPO="https://github.com/polybar/polybar"
 ICON="https://user-images.githubusercontent.com/36028424/39958898-230ddeec-563c-11e8-8318-d658c63ddf22.png"
 EXEC="$APP"
 
-LINUXDEPLOY="https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-static-x86_64.AppImage"
+LINUXDEPLOY="https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20240109-1/linuxdeploy-static-x86_64.AppImage"
 APPIMAGETOOL=$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/[()",{} ]/\n/g' | grep -oi 'https.*continuous.*tool.*x86_64.*mage$' | head -1)
 
 # CREATE DIRECTORIES
