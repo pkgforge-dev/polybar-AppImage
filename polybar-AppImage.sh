9eec1bc9f6ca8ef5550b2e80c851ded9f092b57a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-10-27 17:50:18 -0400

    switch to sharun and bundle everything

--- polybar-AppImage.sh
+++ polybar-AppImage.sh
@@ -7,8 +7,8 @@ REPO="https://github.com/polybar/polybar"
 ICON="https://user-images.githubusercontent.com/36028424/39958898-230ddeec-563c-11e8-8318-d658c63ddf22.png"
 EXEC="$APP"
 
-LINUXDEPLOY="https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20240109-1/linuxdeploy-static-x86_64.AppImage"
 APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
+LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
 
 # CREATE DIRECTORIES
 [ -n "$APP" ] && mkdir -p ./"$APP/$APPDIR" && cd ./"$APP/$APPDIR" || exit 1
@@ -20,27 +20,35 @@ CXXFLAGS='-O3'
 git clone --recursive "$REPO" && cd polybar && mkdir build && cd build && cmake -DENABLE_ALSA=ON .. \
 && make -j$(nproc) && make install DESTDIR="$CURRENTDIR" && cd ../.. || exit 1
 
+exit
+
 # ADD LIBRARIES
 mkdir ./usr/lib ./ & rm -rf ./polybar
+mv ./usr ./shared
+
+wget "$LIB4BN" -O ./lib4bin && wget "$SHARUN" -O ./sharun || exit 1
+chmod +x ./lib4bin ./sharun
+HARD_LINKS=1 ./lib4bin ./shared/bin/* && rm -f ./lib4bin || exit 1
+
 
 # AppRun
 cat >> ./AppRun << 'EOF'
 #!/bin/sh
 CURRENTDIR="$(dirname "$(readlink -f "$0")")"
-export PATH="$PATH:$CURRENTDIR/usr/bin"
+export PATH="$PATH:$CURRENTDIR/bin"
 BIN="$ARGV0"
 unset ARGV0
 case "$BIN" in
 	'polybar'|'polybar-msg')
-		exec "$CURRENTDIR/usr/bin/$BIN" "$@"
+		exec "$CURRENTDIR/bin/$BIN" "$@"
 		;;
 
 	*)
 		if [ "$1" = '--msg' ]; then
 			shift
-			"$CURRENTDIR"/usr/bin/polybar-msg "$@"
+			"$CURRENTDIR"/bin/polybar-msg "$@"
 		else
-			"$CURRENTDIR"/usr/bin/polybar "$@"
+			"$CURRENTDIR"/bin/polybar "$@"
 			echo "AppImage command:"
 			echo " \"$APPIMAGE --msg\"         Launches polybar-msg"
 			echo "You can also symlink the appimage with the name polybar-msg"
@@ -70,9 +78,8 @@ ln -s polybar.png ./.DirIcon
 
 # MAKE APPIMAGE USING FUSE3 COMPATIBLE APPIMAGETOOL
 export ARCH=x86_64
-cd .. && wget "$LINUXDEPLOY" -O linuxdeploy && wget -q "$APPIMAGETOOL" -O ./appimagetool && chmod a+x ./linuxdeploy ./appimagetool \
-&& ./linuxdeploy --appdir "$APPDIR" --executable "$APPDIR"/usr/bin/"$EXEC" || exit 1
+cd .. && wget -q "$APPIMAGETOOL" -O ./appimagetool && chmod +x ./appimagetool || exit 1
 echo "Making appimage"
-./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 ./"$APP".AppDir "$APP"-"$VERSION"-"$ARCH".AppImage
+./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 ./"$APP".AppDir "$APP"-"$VERSION"-"$ARCH".AppImage
 
 [ -n "$APP" ] && mv ./*.AppImage .. && cd .. && rm -rf ./"$APP" && echo "All Done!" || exit 1
